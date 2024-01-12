8f4d8cd61adfae79dc25834cd7d10818c8791a2d
BT_1N3_13 <sample@bths.edu>
2024-01-12 09:17:22 -0500

    almost done with setup + added arraylist to store transactions

--- src/TransactionHistory.java
+++ src/TransactionHistory.java
@@ -1,8 +1,12 @@
+import java.util.ArrayList;
+import java.util.List;
 public class TransactionHistory {
     int idA;
     int idB;
+    ArrayList<String> transactions;
     public TransactionHistory() {
         idA = 0;
         idB = 0;
+        transactions = new ArrayList<>();
     }
 }
